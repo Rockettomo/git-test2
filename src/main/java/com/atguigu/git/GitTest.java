03e25706bb8ece6aee6e6438fb6d3f1649e4a44f
rockect <rockect@atguigu.com>
2022-03-04 15:59:09 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,9 +2,10 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git~");
-        System.out.println("Hello Git~");
-        System.out.println("Hello Git~");
+        System.out.println("Hello Git1~");
+        System.out.println("Hello Git2~");
+        System.out.println("Hello Git2~");
+        System.out.println("Hello Git4~");
 
     }
 }
